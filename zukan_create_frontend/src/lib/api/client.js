84381fef4bc2a81607b072d5ad16fe5d510e10f2
furukoshi12 <syochikubai12@gmail.com>
2023-10-07 15:29:41 +0900

    追加：axiosの設定 baseURLの定義 スネークケースとキャメルケースの変換を定義 ヘッダーのみケバブケースを適用

---
+++ zukan_create_frontend/src/lib/api/client.js
@@ -0,0 +1,13 @@
+import applyCaseMiddleware from "axios-case-converter";
+import axios from "axios";
+
+const options = {
+  ignoreHeaders: true
+}
+
+const client = applyCaseMiddleware(axios.create({
+  baseURL: "http://localhost:3001/api/v1",
+  withCredentials: true
+}), options);
+
+export default client;
