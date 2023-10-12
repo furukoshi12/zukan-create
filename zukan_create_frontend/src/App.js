4eae7f9abe4b1e871572b5b9374265b9d28e0e98
furukoshi12 <syochikubai12@gmail.com>
2023-10-12 23:15:11 +0900

    追加：Topページ 画面遷移を定義

--- zukan_create_frontend/src/App.js
+++ zukan_create_frontend/src/App.js
@@ -1,16 +1,44 @@
 import './App.css';
-//import { Link, Route, Router, Routes } from 'react-router-dom';
+import { Link, Route, BrowserRouter as Router, Routes, useLocation } from 'react-router-dom';
 import LoginForm from './components/LoginForm';
-import Sidebar from './components/Sidebar';
-import TopPage from './components/TopPage';
+import MyPage from './components/MyPage';
+import SignUp from './components/SignUp';
 
 function App() {
   return (
-      <div className="App">
-        <TopPage />
-        <LoginForm />
-        <Sidebar />
-      </div>
+    <Router>
+        <Routes>
+          <Route path="/" element={<TopPage />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<LoginForm />} />
+          <Route path="/mypage" element={<MyPage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+function TopPage(){
+  const location = useLocation();
+  const isTopPage = location.pathname === '/';
+
+  return (
+      <div className={`App ${isTopPage ? 'top-page' : ''}` }>
+        <p className='top-logo'>
+          illustrated<br />
+          book create
+        </p>
+
+      <nav>
+        <ul>
+          <li>
+            <Link to="/signup">SignUp</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
   );
 }
 
