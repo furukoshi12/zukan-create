05e1dc9ccc4c1f4e2c24dc811c223b3375b26608
furukoshi12 <syochikubai12@gmail.com>
2023-10-12 23:20:23 +0900

    追加：サインアップ 入力フォーム追加 バックエンドと通信設定 サインアップ成功後ログインページに遷移

---
+++ zukan_create_frontend/src/components/SignUp.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from 'react'
+import Sidebar from './Sidebar'
+import client from '../lib/api/client';
+import { useNavigate } from 'react-router-dom';
+
+function SignUp(){
+  const history = useNavigate();
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [password_confirmation, setPasswordConfirmation] = useState('');
+  const [error, setError] = useState(null);
+
+  const handleSignUp = async () => {
+    const userData = {
+      user: {
+        name: name,
+        email: email,
+        password: password,
+        password_confirmation: password_confirmation,
+      },
+    };
+
+    client.post('/signup', userData)
+    .then((response) => {
+      console.log('SignUp Success', response.data);
+      history('/login');
+    })
+    .catch((error) => {
+      console.error('SignUp Faild', error.response);
+      setError('サインアップに失敗しました。');
+    });
+  };
+
+  return (
+    <div className='container'>
+      <div className='content'>
+        <Sidebar />
+        <div className='login-form'>
+        {error && <div>{error}</div>}
+          <h1>SignUp</h1>
+          <p>Name</p>
+          <input type='text' placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} />
+          <p>Email</p>
+          <input type='email' placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+          <p>Password</p>
+          <input type='password' placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)} />
+          <p>PasswordConfirmation</p>
+          <input type='password' placeholder='PasswordConfirmation' value={password_confirmation} onChange={(e) => setPasswordConfirmation(e.target.value)} />
+          <button onClick={handleSignUp}>SignUp</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default SignUp;
