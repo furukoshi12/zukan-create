eb262a9452476042c12032c253767fd966ce3e33
furukoshi12 <syochikubai12@gmail.com>
2023-10-12 23:28:08 +0900

    追加：マイページ 図鑑一覧表示 お気に入り一覧表示 表示切り替えボタン

---
+++ zukan_create_frontend/src/components/LikeList.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function LikeList() {
+  return (
+    <div>LikeList</div>
+  )
+}
+
+export default LikeList;
--- zukan_create_frontend/src/components/MyPage.jsx
+++ zukan_create_frontend/src/components/MyPage.jsx
@@ -1,36 +1,40 @@
-import React, { useState } from 'react'
-import Sidebar from './Sidebar'
+import React, { useState } from 'react';
+import Sidebar from './Sidebar';
+import MyPostList from './MyPostList';
+import LikeList from './LikeList';
 
 function MyPage() {
-  const [activeTab, setActiveTab] = useState('post');
+  const [activeTab, setActiveTab] = useState('myPosts');
 
   const handleTabChange = (tabName) => {
     setActiveTab(tabName);
   };
 
   return (
-    <div>
-      <Sidebar />
-      <div className='tabs'>
-        <button
-          onClick={() => handleTabChange('post')}
-          className={activeTab === 'post' ? 'active' : ''}
-        >
-          Post
-        </button>
-        <button
-            onClick={() => handleTabChange('like')}
-            className={activeTab === 'like' ? 'active' : ''}
+    <div className='container'>
+      <div className='content'>
+        <Sidebar />
+        <div className='tabs'>
+          <button
+            onClick={() => handleTabChange('myPosts')}
+            className={activeTab === 'myPosts' ? 'active' : ''}
           >
-            Like
-        </button>
-      </div>
-      <div className='tab-content'>
-        {activeTab === 'post' && <PostList />}
-        {activeTab === 'like' && <LikeList />}
+            Post
+          </button>
+          <button
+              onClick={() => handleTabChange('like')}
+              className={activeTab === 'like' ? 'active' : ''}
+            >
+              Like
+          </button>
+        </div>
+        <div className='tab-content'>
+          {activeTab === 'myPosts' && <MyPostList />}
+          {activeTab === 'like' && <LikeList />}
+        </div>
       </div>
     </div>
   );
 }
 
-export default MyPage
+export default MyPage;
