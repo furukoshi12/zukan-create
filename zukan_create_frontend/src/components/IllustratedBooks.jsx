650e06ff848adba3c634d68f0e04d192f8d3108c
furukoshi12 <syochikubai12@gmail.com>
2023-10-26 00:12:34 +0900

    追加：Illustrated_books一覧ページ みんなの図鑑の一覧ページ追加 サイドバーにリンクを追加

---
+++ zukan_create_frontend/src/components/IllustratedBooks.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react'
+import Sidebar from './Sidebar'
+import client from '../lib/api/client'
+import LikeButton from './LikeButton';
+
+function IllustratedBooks(){
+  const [illustratedBooks, setIllustratedBooks] = useState([]);
+
+  useEffect(() => {
+    client.get('/illustrated_books')
+    .then((response) => {
+      console.log(response)
+      setIllustratedBooks(response.data.data)
+    })
+    .catch((error) => {
+      console.log('API_reuest_error', error);
+    });
+  }, []);
+
+  return (
+    <div className='container'>
+      <div className='content'>
+      <Sidebar />
+        <div className='card-container'>
+          <ul>
+            {illustratedBooks.map(illustratedBook => (
+              <li key={illustratedBook.id} className='illustrated-book-card'>
+                <p className='title'>{illustratedBook.attributes.title}</p>
+                <LikeButton illustratedBookId={illustratedBook.id}/>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default IllustratedBooks;
