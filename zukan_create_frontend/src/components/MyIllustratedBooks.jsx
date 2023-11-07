3c43c301f1b7220552066a9fcc969057ef7ecb72
furukoshi12 <syochikubai12@gmail.com>
2023-11-07 22:34:50 +0900

    追加：deleteボタン追加 自分の図鑑を削除するボタンを追加

--- zukan_create_frontend/src/components/MyIllustratedBooks.jsx
+++ zukan_create_frontend/src/components/MyIllustratedBooks.jsx
@@ -1,14 +1,30 @@
 import React from 'react';
 import LikeButton from './LikeButton';
+import client from '../lib/api/client';
+import { Delete } from '@mui/icons-material';
 
 function MyIllustratedBooks({ illustratedBooks }) {
+  const handleDelete = (illustratedBookId) => {
+    client.delete(`/user/illustrated_books/${illustratedBookId}`)
+    .then(() =>{
+      window.location.reload();
+    })
+    .catch (error => {
+      console.error("削除中にエラーが発生しました: ", error);
+    });
+  }
+  
   return (
     <div className='card-container'>
       <ul>
         {illustratedBooks.map(illustratedBook => (
           <li key={illustratedBook.id} className='illustrated-book-card'>
             <p className='title'>{illustratedBook.attributes.title}</p>
+            <p className='tag'>{illustratedBook.attributes.tags}</p>
             <LikeButton illustratedBookId={illustratedBook.id}/>
+            <button type="submit" onClick={() => handleDelete(illustratedBook.id)}>
+              <Delete />
+            </button>
           </li>
         ))}
       </ul>
