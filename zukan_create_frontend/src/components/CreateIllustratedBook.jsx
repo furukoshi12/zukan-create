1efca014c57af595444bff3fc03ce6e850291786
furukoshi12 <syochikubai12@gmail.com>
2023-11-07 22:33:07 +0900

    追加：図鑑作成画面 titleの作成 Tagの追加（サジェスト機能）

--- zukan_create_frontend/src/components/CreateIllustratedBook.jsx
+++ zukan_create_frontend/src/components/CreateIllustratedBook.jsx
@@ -1,50 +1,73 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Sidebar from './Sidebar';
 import client from '../lib/api/client';
-import ReactTagInput from '@pathofdev/react-tag-input';
+import { WithContext as ReactTags } from 'react-tag-input';
 
 export const CreateIllustratedBook = () => {
   const [title, setTitle] = useState('');
+  const [suggestions, setSuggestions] = useState([]);
   const [tags, setTags] = useState([]);
+  const deleteDoubleArray = [...new Set(tags.map(tag => tag.text))];
+  const tagStr = deleteDoubleArray.join(' ');
   const history = useNavigate();
+
+  useEffect(() => {
+    const fetchTags = async () => {
+      try {
+        const response = await client.get('/tags');
+        const formattedData = response.data.data.map(tag => ({
+          id: tag.id,
+          text: tag.attributes.name
+        }));
+        setSuggestions(formattedData);
+      } catch (error) {
+        console.error('Error fetching tags: ', error);
+      }
+    };
+    fetchTags();
+  }, []);
 
   const onClickHome = () => {
     history("/mypage");
+  };
+
+  const generateParams = {
+    title: title,
+    tags: tagStr,
   };
 
   const handleChange = (e) => {
     setTitle(e.target.value);
   };
 
-  const handleSubmit = async (e) => {
-    console.log(e)
-    const deleteDoubleArray = [...new Set(tags)];
-    const tagStr = deleteDoubleArray.join(' ');
-    
-    const generateParams = {
-      title: title,
-      tags: tagStr,
-    };
+  const handleDelete = (i) => {
+    const newTags = [...tags];
+    newTags.splice(i, 1);
+    setTags(newTags);
+  };
 
+  const handleAddition = (tag) => {
+    setTags([...tags, tag]);
+  };
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
     try {
       const response = await client.post('/user/illustrated_books', generateParams);
-      console.log('tag', response)
       setTitle("");
       setTags([]);
-      //history("/mypage");
+      history("/mypage");
     } catch (error) {
       console.log(error);
     }
   };
 
-
   return (
     <div className='container'>
       <div className='content'>
         <Sidebar />
-        <form onSubmit={(e) => handleSubmit(e)}>
+        <form>
           <input
             mb="24px"
             type="text"
@@ -52,13 +75,14 @@ export const CreateIllustratedBook = () => {
             onChange={(e) => handleChange(e)}
             value={title}
           />
-          <ReactTagInput
-            placeholder="タグを入力してください"
+          <ReactTags
+            placeholder="Enterでタグ追加"
             tags={tags}
-            onChange={(e) => setTags(e)}
-            allowUnique={true}
+            suggestions={suggestions}
+            handleDelete={(i) => handleDelete(i)}
+            handleAddition={(tag) => handleAddition(tag)}
           />
-          <button type="submit">
+          <button type="submit" onClick={(e) => handleSubmit(e)}>
             投稿
           </button>
           <button onClick={onClickHome}>
@@ -67,7 +91,7 @@ export const CreateIllustratedBook = () => {
         </form>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default CreateIllustratedBook
+export default CreateIllustratedBook;
