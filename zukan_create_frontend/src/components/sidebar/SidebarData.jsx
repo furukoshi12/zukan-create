f7b9d04378914d630de2e75646ae27de3ead3d2e
furukoshi12 <syochikubai12@gmail.com>
2023-11-26 23:41:56 +0900

    編集 sidebar関連のコンポーネントを専用フォルダに移動 Sidebar:Modalをインポート SidebarData:データのリストの表示順を調整

---
+++ zukan_create_frontend/src/components/sidebar/SidebarData.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import {
+  Home,
+  Mail as MailIcon,
+  Grading as GradingIcon,
+  List as ListIcon,
+  Create as CreateIcon,
+} from '@mui/icons-material';
+
+export const SidebarData = [
+  {
+    title: "MyPage",
+    icon: <Home />,
+    link: "/mypage",
+  },
+  {
+    title: "Everyone's IllustratedBooks",
+    icon: <ListIcon />,
+    link: "/illustratedbooks",
+  },
+  {
+    title: "Create New IllustratedBook",
+    icon: <CreateIcon />,
+    link: "/new",
+  },
+];
+
+export const SidebarFooterData = [
+  {
+    title: "Regulation",
+    icon: <GradingIcon />,
+    link: "/regulation",
+  },
+  {
+    title: "Mail",
+    icon: <MailIcon />,
+    link: "/mail",
+  }
+];
