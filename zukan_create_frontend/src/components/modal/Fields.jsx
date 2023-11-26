39ec323aa9de91caf09b7bbcea6c842f7784ed81
furukoshi12 <syochikubai12@gmail.com>
2023-11-26 23:37:47 +0900

    追加 図鑑作成ページのモーダルの追加 Modal:モーダルの動作を定義（ReactModal使用） Feilds:APIから入力フィールド一覧取得 　　　 選択した入力フィールドの値を図鑑作成ページで送信

---
+++ zukan_create_frontend/src/components/modal/Fields.jsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from 'react'
+import client from '../../lib/api/client';
+
+function Fields({ onAddInput }) {
+  const [fieldDesigns, setFieldDesigns] = useState([]);
+
+  useEffect(() => {
+    client.get('/field_designs')
+      .then((response) => {
+        console.log(response)
+        setFieldDesigns(response.data.data)
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  },[])
+
+  const handleSelectInput = (inputData) => {
+    onAddInput(inputData);
+  };
+
+  return (
+    <div className='container' style={{backgroundColor: "white"}}>
+      <ul>
+        {fieldDesigns.map(fieldDesign => (
+          <li key={fieldDesign.id} className='field-card' onClick={() => handleSelectInput(fieldDesign)}>
+            <label>{fieldDesign.attributes.label}</label>
+            <textarea
+              type="text"
+              style={{
+                backgroundColor: fieldDesign.attributes.backgroundColor,
+                color: fieldDesign.attributes.color,
+                borderColor: fieldDesign.attributes.borderColor,
+                borderStyle: fieldDesign.attributes.borderStyle,
+                borderRadius: fieldDesign.attributes.borderRadius,
+                fontFamily: fieldDesign.attributes.fontFamily,
+                fontSize: fieldDesign.attributes.fontSize
+              }}
+            />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Fields
