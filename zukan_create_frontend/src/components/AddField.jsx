8799125df84192f5dce5479161111a176284d42e
furukoshi12 <syochikubai12@gmail.com>
2023-12-17 15:56:58 +0900

    追加 AddField： 入力フィールドのドラッグ&ドロップを追加 onUpdatePositionで移動後のポジションを送信 draggableAreaからはみ出た要素を初期位置に戻す uuidで要素を識別

--- zukan_create_frontend/src/components/AddField.jsx
+++ zukan_create_frontend/src/components/AddField.jsx
@@ -1,15 +1,55 @@
-import React from 'react'
+import interact from 'interactjs';
+import React, { useEffect } from 'react'
 
-const AddField = ({ data }) => {
-  console.log('Add', data)
+const AddField = ({ data, onUpdatePosition }) => {
+  useEffect(() => {
+    interact('.field-card')
+      .draggable({
+      onstart: (event) => {
+        event.target.style.willChange = 'transform';
+      },
+      onmove: event => {
+        requestAnimationFrame(() => {
+        const target = event.target;
+        const x = (parseFloat(target.dataset.x) || 0 ) + event.dx;
+        const y = (parseFloat(target.dataset.y) || 0 ) + event.dy;
+        target.style.transform = `translate(${x}px, ${y}px)`;
+        target.dataset.x = x;
+        target.dataset.y = y;
+        });
+      },
+      onend: (event) => {
+        event.target.style.willChange = 'auto';
+      },
+    })
+    .on('dragend', (event) => {
+      const target = event.target;
+      const uuid = target.getAttribute('data-id');
+      const x = parseFloat(target.getAttribute('data-x')) || 0;
+      const y = parseFloat(target.getAttribute('data-y')) || 0;
+      let finalX = x;
+      let finalY = y;
+
+      const draggableArea = document.querySelector('.draggable-area');
+
+      if (x < 0 || y < 0 || x + target.offsetWidth > draggableArea.offsetWidth || y + target.offsetHeight > draggableArea.offsetHeight) {
+        target.style.transform = 'translate(0px, 0px)';
+        finalX = 0;
+        finalY = 0;
+        target.setAttribute('data-x', finalX);
+        target.setAttribute('data-y', finalY);
+      }
+      onUpdatePosition(uuid, finalX, finalY);
+    });
+  }, [data, onUpdatePosition]);
   
   return (
     <div>
       <div>
         {data && (
           <ul>
-            {data.map(fieldDesign => (
-              <li key={fieldDesign.id} className='field-card'>
+            {data.map((fieldDesign, index) => (
+              <li key={index} data-id={fieldDesign.uuid} className='field-card' style={{position: 'absolute', top: `${fieldDesign.y}px`, left:`${fieldDesign.x}px` }}>
                 <label>{fieldDesign.attributes.label}</label>
                 <textarea
                   type="text"
@@ -20,7 +60,7 @@ const AddField = ({ data }) => {
                     borderStyle: fieldDesign.attributes.borderStyle,
                     borderRadius: fieldDesign.attributes.borderRadius,
                     fontFamily: fieldDesign.attributes.fontFamily,
-                    fontSize: fieldDesign.attributes.fontSize
+                    fontSize: fieldDesign.attributes.fontSize,
                   }}
                 />
               </li>
