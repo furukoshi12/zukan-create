90ae5cab708913faeb52fda62d22dbdcc01133cc
furukoshi12 <syochikubai12@gmail.com>
2023-12-17 16:02:14 +0900

    追加 AddTemplate： 別コンポーネントで選択されたテンプレートのデータを受け取る 受け取ったデータを整理してフィールドの種類と位置を再現

---
+++ zukan_create_frontend/src/components/AddTemplate.jsx
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from 'react'
+
+function AddTemplate({ templateData }) {
+  const [inputs, setInputs] = useState([]);
+
+  useEffect (() => {
+    if (templateData) {
+      const templateFieldDesigns = templateData.templateFieldDesigns
+      const fieldDesigns = templateData.fieldDesigns;
+
+      const inputsWithPositionAndStyle = templateFieldDesigns.map((design) => {
+        const fieldDesign = fieldDesigns.find((fd) => fd.id === design.relationships.fieldDesign.data.id);
+
+        return {
+          ...fieldDesign.attributes,
+          templateId: design.relationships.template.data.id,
+          xPosition: design.attributes.xPosition,
+          yPosition: design.attributes.yPosition,
+        };
+      });
+
+      setInputs(inputsWithPositionAndStyle);
+    }
+  }, [templateData])
+
+
+
+  return (
+    <div>
+      {templateData && (
+          <div key={templateData.id} className='template-card'>
+            <ul>
+              {inputs.map(input =>(
+                  <li key={input.uuid} style={{ position: 'absolute', left: input.xPosition, top: input.yPosition }} >
+                    <label>{input.label}</label>
+                    <textarea
+                      type="text"
+                      style={{
+                        backgroundColor: input.backgroundColor,
+                        color: input.color,
+                        borderColor: input.borderColor,
+                        borderStyle: input.borderStyle,
+                        borderRadius: input.borderRadius,
+                        fontFamily: input.fontFamily,
+                        fontSize: input.fontSize,
+                      }}
+                    />
+                  </li>
+                ))}
+            </ul>
+          </div>
+      )}
+    </div>
+  );
+}
+
+export default AddTemplate;
