72fecf893a97f03bb1de8a00eb5ac8c764d9b664
furukoshi12 <syochikubai12@gmail.com>
2024-01-02 01:11:08 +0900

    追加 CreateTemplate, CreateField: template, field_design作成ページのコードをこちらに移動

---
+++ zukan_create_frontend/src/components/dashboard/CreateTemplate.jsx
@@ -0,0 +1,107 @@
+import React, { useState, useEffect, useRef } from 'react';
+import AddField from '../AddField';
+import client from '../../lib/api/client';
+import AdminSidebar from './sidebar/AdminSidebar'
+import { v4 as uuidv4 } from 'uuid';
+import AddTemplate from '../AddTemplate';
+
+const CreateTemplate = () => {
+  const [name, setName] = useState('');
+  const [templateInputs, setTemplateInputs] = useState([]);
+  const [template, setTemplate] = useState(null)
+  const templateRef = useRef(null);
+  const [areaSize, setAreaSize] = useState({ width: 0, height: 0 });
+
+  useEffect(() => {
+    const updateSize = () => {
+      if (templateRef.current) {
+        setAreaSize({
+          width: templateRef.current.offsetWidth,
+          height: templateRef.current.offsetHeight,
+        });
+      }
+    };
+  
+    window.addEventListener('resize', updateSize);
+    updateSize();
+    return () => window.removeEventListener('resize', updateSize);
+  }, []);
+  
+  const handleAddTemplate = (templateData) => {
+    setTemplate(templateData)
+  };
+
+  const addInputToTemplate = (inputData) => {
+    const uuid = inputData.uuid || uuidv4();
+    setTemplateInputs((prevInputs) => [
+      ...prevInputs,
+      { ...inputData, x: 0, y: 0, width: null, height: null, uuid: uuid},
+    ]);
+  };
+
+  const updateInputPosition = ( uuid, x, y) => {
+    setTemplateInputs((prevInputs) => {
+      return prevInputs.map((input) => {
+        if(input.uuid === uuid) {
+          return { ...input, x, y};
+        }
+        return input;
+      });
+    });
+  };
+
+  const updateInputSize = ( uuid, width, height) => {
+    setTemplateInputs((prevInputs) => {
+      return prevInputs.map((input) => {
+        if(input.uuid === uuid) {
+          return { ...input, width, height};
+        }
+        return input;
+      });
+    });
+  };
+
+  const handleName = (e) => {
+    setName(e.target.value);
+  };
+
+  const templateParams = {
+    template: {
+      name: name,
+      template_field_designs_attributes: templateInputs.map(input => ({
+        field_design_id: input.id,
+        x_position: input.x / areaSize.width,
+        y_position: input.y / areaSize.height,
+        width: input.width / areaSize.width,
+        height: input.height / areaSize.height,
+      }))
+    },
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try{
+      await client.post('/templates', templateParams);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className='container'>
+      <AdminSidebar onAddInput={addInputToTemplate} onAddTemplate={handleAddTemplate} />
+      <div className='content'>
+        <form onSubmit={(e) => handleSubmit(e)}>
+          <input type='text' placeholder="Template Name" onChange={(e) => handleName(e)} value={name} />
+          <div className="draggable-area" ref={templateRef}>
+            <AddTemplate areaSize={areaSize} templateData={template} onUpdatePosition={updateInputPosition} onUpdateSize={updateInputSize}/>
+            <AddField data={templateInputs} onUpdatePosition={updateInputPosition} onUpdateSize={updateInputSize}/>
+          </div>
+          <button type='submit'>Create Template</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default CreateTemplate;
