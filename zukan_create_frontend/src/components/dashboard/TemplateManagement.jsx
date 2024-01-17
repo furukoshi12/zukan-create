a732a25b7fd10927afa9027017370a747a0e70b9
furukoshi12 <syochikubai12@gmail.com>
2024-01-18 00:36:56 +0900

    retry

--- zukan_create_frontend/src/components/dashboard/TemplateManagement.jsx
+++ zukan_create_frontend/src/components/dashboard/TemplateManagement.jsx
@@ -13,7 +13,7 @@ export const TemplateManagement = () => {
       setTemplates(response.data.data)
     })
     .catch((error) => {
-      console.log(error);
+      console.error(error);
     });
   }, []);
 
